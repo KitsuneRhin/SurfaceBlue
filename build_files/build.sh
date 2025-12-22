bb60911796902b5db606e0540b61b8a493a0fedb
KitsuneRhin <122936230+KitsuneRhin@users.noreply.github.com>
2025-12-22 12:03:57 -0800

    Surface-Linux Integration
    Adds surface-linux kernel and drivers to build.sh install routine.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,21 @@
 
 set -ouex pipefail
 
+### Add linux-surface repo (Fedora42)
+cat >/etc/yum.repos.d/linux-surface.repo << 'EOF'
+[linux-surface]
+name=linux-surface
+baseurl=https://pkg.surfacelinux.com/fedora/f42
+enabled=1
+skip_if_unavailable=False
+gpgkey=https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc
+gpgcheck=1
+enabled_metadata=1
+type=rpm-md
+repo_gpgcheck=0
+EOF
+
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -9,8 +24,21 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+
+
 # this installs a package from fedora repos
 dnf5 install -y tmux 
+
+# Surface-specific kernel and helpers
+dnf5 install -y \
+kernel-surface \
+iptsd \
+libwacom-surface \
+libwacom-surface-data
+
+# Enable services
+systemctl enable podman.socket
+systemctl enable iptsd
 
 # Use a COPR Example:
 #
@@ -19,6 +47,3 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
